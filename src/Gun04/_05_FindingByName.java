1318bce02883a50a7dcd0209b4dc431f8c00937e
isakov-m <chyraktar@gmail.com>
2025-01-21 01:09:25 +0600

    Selenium Gun04 c3

---
+++ src/Gun04/_05_FindingByName.java
@@ -0,0 +1,43 @@
+package Gun04;
+
+import Utility.MyFunc;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class _05_FindingByName {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://form.jotform.com/221934510376353");
+
+        // textBox lari name ile bulup adinizi ve soyadinizi doldurun
+        // ve butonu da id veya name ile bulup tiklatin
+
+        WebElement firstName = driver.findElement(By.name("q8_name[first]"));   // name ile bulma
+        firstName.sendKeys("Ismet");
+
+
+        WebElement lasttName = driver.findElement(By.name("q8_name[last]"));    // name ile bulma
+        lasttName.sendKeys("Temur");
+
+        WebElement clickBtn = driver.findElement(By.id("input_2"));
+        clickBtn.click();
+
+
+
+        MyFunc.Wait(5);
+        driver.quit();
+
+
+
+
+
+
+
+
+
+
+    }
+}
