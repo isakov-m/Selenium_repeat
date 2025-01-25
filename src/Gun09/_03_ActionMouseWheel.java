4a0fc77ffc19c5b2b28598a4e0991652d5ecbef6
isakov-m <chyraktar@gmail.com>
2025-01-25 21:09:43 +0600

    Selenium Gun09 Action, MouseWheel

---
+++ src/Gun09/_03_ActionMouseWheel.java
@@ -0,0 +1,50 @@
+package Gun09;
+
+import Utility.BaseDriver;
+import Utility.MyFunc;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+public class _03_ActionMouseWheel extends BaseDriver {
+    // mousun doner topu (Wheel) kaydirma
+
+    @Test
+    public void Action_mw_ts1(){
+        // elemente scroll
+        driver.get("https://www.selenium.dev/selenium/web/scrolling_tests/frame_with_nested_scrolling_frame_out_of_view.html");
+        WebElement iframe = driver.findElement(By.name("nested_scrolling_frame"));
+
+        MyFunc.Wait(3);
+        // iframe kadar scroll yap
+        new Actions(driver).scrollToElement(iframe).build().perform();
+
+        BekleVeKapat();
+    }
+
+    @Test
+    public void Action_mw_ts2(){
+        // belli bir miktar kadar scroll
+        driver.get("https://www.selenium.dev/selenium/web/scrolling_tests/frame_with_nested_scrolling_frame_out_of_view.html");
+        MyFunc.Wait(3);
+
+        new Actions(driver).scrollByAmount(0,500).build().perform();
+
+        BekleVeKapat();
+    }
+
+    @Test
+    public void Action_mw_ts3(){
+        // belli bir miktar kadar scroll
+        driver.get("https://www.selenium.dev/selenium/web/scrolling_tests/frame_with_nested_scrolling_frame_out_of_view.html");
+        MyFunc.Wait(3);
+        WebElement iframe = driver.findElement(By.name("nested_scrolling_frame"));
+
+        // TODO : iframe kadar 100px 100px seklinde adim adim 1 sn araliklarla scroll islemi yapiniz
+        // ipucu elementin sayfanin basindan nekadar asagida oldugu bilgisini
+        // element.getReact().getY;     seklinde alabilirsiniz
+
+        BekleVeKapat();
+    }
+}
