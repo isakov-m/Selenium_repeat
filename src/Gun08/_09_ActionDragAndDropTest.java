f97a0701f6d3e8ffef12e7d97c052f14e2ee8b9d
isakov-m <chyraktar@gmail.com>
2025-01-25 18:34:46 +0600

    Selenium Gun08 Action, DragAndDrop

---
+++ src/Gun08/_09_ActionDragAndDropTest.java
@@ -0,0 +1,35 @@
+package Gun08;
+
+import Utility.BaseDriver;
+import Utility.MyFunc;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+public class _09_ActionDragAndDropTest extends BaseDriver {
+    @Test
+    public void Test1(){
+        driver.get("http://www.dhtmlgoodies.com/scripts/drag-drop-custom/demo-drag-drop-3.html");
+        Actions driverAksiyon = new Actions(driver);
+
+        MyFunc.Wait(2);
+        WebElement oslo=driver.findElement(By.id("box1"));
+        WebElement norway=driver.findElement(By.id("box101"));
+
+        //1.yöntem
+        driverAksiyon.dragAndDrop(oslo, norway).build().perform();
+        // oslo yu al norway e götür bırak
+
+        //2.Yöntem : 2 aşamalı
+        driverAksiyon.clickAndHold(oslo).build().perform(); // osloyu eline al.
+        driverAksiyon.moveToElement(norway).release().build().perform(); // norway in üzerine git bırak
+
+        BekleVeKapat();
+
+
+
+
+
+    }
+}
